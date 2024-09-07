66e89d8fc86663488d7f5003da62f434c9c4ad87
darlin <darlinmanuelcasado@gmail.com>
2024-09-07 12:12:00 -0400

    New project added

--- public/data/projects.js
+++ public/data/projects.js
@@ -8,9 +8,10 @@ const projects = [
     },
     {
         id: 2,
-        title: "Project title",
-        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla malesuada leo at luctus dignissim. Etiam a dolor vel felis mattis imperdiet. Cras consectetur urna quis nibh eleifend, eget dictum ligula sollicitudin.",
-        imgUrl: "./images/LaughLabLogo.png"
+        title: "FlashNotes",
+        description:"A quick and intuitive app for capturing, organizing, and managing notes, tasks, and ideas. Its sleek design makes note-taking fast, simple, and enjoyable.",
+        imgUrl: "./images/FlashNotes.png",
+        url: "https://notes.darlincasado.lat/"
     },
     {
         id: 3,
